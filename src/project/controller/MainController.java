08af9248b563469cc623aff8626fddc9f947f290
buelee87 <buelee87@gmail.com>
2016-09-21 12:50:02 +0900

    Model 추가 작업

--- src/project/controller/MainController.java
+++ src/project/controller/MainController.java
@@ -12,7 +12,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
-import project.Model.*;
+import project.model.*;
 
 import java.net.URL;
 import java.time.LocalDate;
